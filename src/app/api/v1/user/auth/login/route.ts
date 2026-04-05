3c2577e0c1a81a4a24d06986e08753aab507b498
uchechigeorge <73939690+uchechigeorge@users.noreply.github.com>
2026-04-05 23:57:02 +0100

    fix: add cors directly to login

--- src/app/api/v1/user/auth/login/route.ts
+++ src/app/api/v1/user/auth/login/route.ts
@@ -17,7 +17,7 @@ export const OPTIONS = () =>
   new Response(null, {
     status: 204,
     headers: {
-      "Access-Control-Allow-Origin": "localhost:4200",
+      "Access-Control-Allow-Origin": "http://localhost:4200",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Credentials": "true",
