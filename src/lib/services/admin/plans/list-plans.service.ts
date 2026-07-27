3cda36ba2e31dd35525f4850da3e164e1fe51f37
user <mimivoxai@gmail.com>
2026-07-27 08:21:44 +0100

    feat: add pricings to admin plan dto

--- src/lib/services/admin/plans/list-plans.service.ts
+++ src/lib/services/admin/plans/list-plans.service.ts
@@ -7,9 +7,12 @@ import { planReadDtoValidator } from "@/lib/validators/admin/plan.validator";
 export const listPlans = async (
   params: PlanListParams,
 ): Promise<[PlanReadDto[], ListPlansMetaResponse]> => {
-  const [data, total] = await planRepo.query({
-    ...params,
-  });
+  const [data, total] = await planRepo.query(
+    {
+      ...params,
+    },
+    { includeRelations: true },
+  );
 
   const dto: PlanReadDto[] = await parseArr(data, planReadDtoValidator);
 
